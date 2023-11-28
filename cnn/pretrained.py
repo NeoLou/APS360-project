8ea2c479ba6d7efb874b6303c92ef613748a6699
Neo Lou <neolou2000@hotmail.com>
2023-11-28 17:08:41 -0500

    tested model on test set

--- cnn/pretrained.py
+++ cnn/pretrained.py
@@ -83,6 +83,30 @@ def get_model_name(name, batch_size, learning_rate, num_epochs, epoch):
     path = f"./training/{name}_{batch_size}_{learning_rate}_{num_epochs}_{epoch}"
     return path
 
+def visualize_model(best_model, dataloader, num_images=6):
+    for batch in dataloader:
+        img, label = batch
+        if torch.cuda.is_available():
+            img = img.cuda()
+            label = label.cuda()
+        outputs = best_model(img)
+        outputs = outputs.squeeze()
+    
+    for i in range(num_images):
+        cur_img = img[i].cpu().numpy()
+        cur_label = int(label[i].cpu().numpy() * 4991 + 1)
+        cur_output = int(outputs[i].cpu().detach().numpy() * 4991 + 1)
+        cur_img = cur_img.transpose((1, 2, 0))
+        plt.subplot(2, 3, i+1)
+        plt.imshow(cur_img)
+        plt.title(f"Label:{cur_label}, Prediction: {cur_output}")
+        plt.axis('off')
+    plt.suptitle("Model predictions for poopularity ranks")
+    plt.savefig("./cnn/model_predictions")
+    plt.show()
+    plt.close()
+    
+
 if __name__ == '__main__':
     # Get the device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -139,25 +163,9 @@ if __name__ == '__main__':
             pretrained_params = {'lr': lr, 'num_epochs': nums_epochs, 'batch_size': batch_size, 'name': name}
             # Train the model
             torch.cuda.empty_cache()
-            model.train_model(cur_model, train_loader, valid_loader,
-                              pretrained=True, pretrained_params=pretrained_params)
-    # Trained efficient net with: exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
-    # Changed to step_size=40, gamma=0.5 for other models
+            #model.train_model(cur_model, train_loader, valid_loader,
+            #                  pretrained=True, pretrained_params=pretrained_params)
     
-    
-    
-    
-    # Apply it to the input image
-    # batch = preprocess(img).unsqueeze(0)
-    # # Use the model and print the predicted category
-    # prediction = model(batch).squeeze(0).softmax(0)
-    # class_id = prediction.argmax().item()
-    # score = prediction[class_id].item()
-    # category_name = weights.meta["categories"][class_id]
-    # print(f"{category_name}: {100 * score:.1f}%")
-    
-    # visualize_model(model)
-
     # img, label = pickle.loads(pickle.load(open('./data_collection/img_data/images/0', 'rb')))
     # transform = transforms.RandomCrop(450, pad_if_needed=True)
     # img = transform(img)
@@ -166,23 +174,29 @@ if __name__ == '__main__':
     # print(outputs)
     
     # Load best model
-    # name = "shufflenet_v2_x1_5"
-    # bs = 128
-    # lr = 0.001
-    # num_epochs = 100
-    # best_epoch = 37
-    # cur_model = shufflenet_v2_x1_5(weights=None)
-    # if name == 'shufflenet_v2_x1_5':
-    #     num_features = cur_model.fc.in_features
-    #     cur_model.fc = nn.Linear(num_features, 1)
-    # else:
-    #     num_features = cur_model.classifier[1].in_features
-    #     cur_model.classifier[1] = nn.Linear(num_features, 1)
-    # model_path = get_model_name(name=name, batch_size=bs,
-    #                             learning_rate=lr, num_epochs=num_epochs, epoch=best_epoch)
-    # state = torch.load(model_path)
-    # cur_model.load_state_dict(state)
-    # cur_model = cur_model.to(device) # Move model to the GPU
-    # # Evaluate the model on test set
-    # test_loss = evaluate.evaluate(cur_model, test_loader, pretrained=True)
-    # print(f"Test loss: {test_loss}")
+    name = "shufflenet_v2_x1_5"
+    bs = 128
+    lr = 0.001
+    num_epochs = 100
+    best_epoch = 37
+    cur_model = shufflenet_v2_x1_5(weights=None)
+    if name == 'shufflenet_v2_x1_5':
+        num_features = cur_model.fc.in_features
+        cur_model.fc = nn.Linear(num_features, 1)
+    else:
+        num_features = cur_model.classifier[1].in_features
+        cur_model.classifier[1] = nn.Linear(num_features, 1)
+    model_path = get_model_name(name=name, batch_size=bs,
+                                learning_rate=lr, num_epochs=num_epochs, epoch=best_epoch)
+    state = torch.load(model_path)
+    cur_model.load_state_dict(state)
+    cur_model = cur_model.to(device) # Move model to the GPU
+    # Get test loader
+    weights = ShuffleNet_V2_X1_5_Weights.IMAGENET1K_V1
+    preprocess = weights.transforms()
+    train_loader, valid_loader, test_loader = get_data_loader(batch_size=bs, preprocess=preprocess)
+    # Evaluate the model on test set
+    test_loss = evaluate.evaluate(cur_model, test_loader, pretrained=True)
+    print(f"Test loss: {test_loss}") # Test loss: 0.22590143233537674
+    
+    visualize_model(cur_model, test_loader)
